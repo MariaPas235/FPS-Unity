eab4f4779ecadccdafa423ca9fe123425150361d
MariaPas235 <151913868+MariaPas235@users.noreply.github.com>
2024-10-19 16:17:43 +0200

    Contador balas y vida, modelo enemigos

--- Assets/Script/World/GameManager.cs
+++ Assets/Script/World/GameManager.cs
@@ -1,26 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
+    public Text ammoText;
     public static GameManager Instance { get; private set; }
 
     public int gunAmmo = 10;
 
+    public Text healthText ;
+
+    public int health = 100;
+
     private void Awake()
     {
-        // Si ya existe una instancia y no es esta, destruye este objeto
+        
         if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
         }
         else
         {
-            // Asigna esta instancia como la instancia única
+            
             Instance = this;
-            // Si quieres que el GameManager persista entre escenas
+           
             DontDestroyOnLoad(gameObject);
         }
+    }
+
+    private void Update()
+    {
+        ammoText.text = gunAmmo.ToString();
+        healthText.text = health.ToString();
     }
 }
